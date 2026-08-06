2a81641cdfea6e75742cdf6dd6863feb0f787c43
mogoodrich <mgoodrich@pih.org>
2026-08-06 09:57:31 -0400

    test: add orderPoller unit tests

---
+++ test/lib/orderPoller.test.js
@@ -0,0 +1,129 @@
+jest.mock('axios');
+jest.mock('../../src/lib/logger', () => ({ info: jest.fn(), warn: jest.fn(), error: jest.fn() }));
+jest.mock('../../src/lib/openmrsClient');
+
+let axios;
+
+describe('orderPoller', () => {
+  let openmrs;
+  let orderPoller;
+
+  beforeEach(() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+    jest.useFakeTimers();
+
+    process.env.OPENHIM_ROUTER_URL = 'http://openhim-core:5001';
+
+    axios = require('axios');
+    openmrs = require('../../src/lib/openmrsClient');
+    orderPoller = require('../../src/lib/orderPoller');
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  test('pollOnce POSTs each entry in the search bundle to the OpenHIM inbound channel', async () => {
+    const sr1 = { resourceType: 'ServiceRequest', id: 'sr1' };
+    const sr2 = { resourceType: 'ServiceRequest', id: 'sr2' };
+    openmrs.searchServiceRequests.mockResolvedValue({ entry: [{ resource: sr1 }, { resource: sr2 }] });
+    axios.post.mockResolvedValue({ status: 200 });
+
+    await orderPoller.pollOnce();
+
+    expect(axios.post).toHaveBeenCalledWith(
+      'http://openhim-core:5001/fhir/ServiceRequest',
+      sr1,
+      { headers: { 'Content-Type': 'application/fhir+json' } }
+    );
+    expect(axios.post).toHaveBeenCalledWith(
+      'http://openhim-core:5001/fhir/ServiceRequest',
+      sr2,
+      { headers: { 'Content-Type': 'application/fhir+json' } }
+    );
+    expect(axios.post).toHaveBeenCalledTimes(2);
+  });
+
+  test('one entry failing to submit does not stop the remaining entries', async () => {
+    const sr1 = { resourceType: 'ServiceRequest', id: 'sr1' };
+    const sr2 = { resourceType: 'ServiceRequest', id: 'sr2' };
+    openmrs.searchServiceRequests.mockResolvedValue({ entry: [{ resource: sr1 }, { resource: sr2 }] });
+    axios.post
+      .mockRejectedValueOnce(new Error('502 from OpenHIM'))
+      .mockResolvedValueOnce({ status: 200 });
+
+    await orderPoller.pollOnce();
+
+    expect(axios.post).toHaveBeenCalledTimes(2);
+  });
+
+  test('the first pollOnce searches with no lastUpdatedAfter cursor', async () => {
+    openmrs.searchServiceRequests.mockResolvedValue({ entry: [] });
+
+    await orderPoller.pollOnce();
+
+    expect(openmrs.searchServiceRequests).toHaveBeenCalledWith({ lastUpdatedAfter: null });
+  });
+
+  test("a second pollOnce searches with the previous call's timestamp as the cursor", async () => {
+    jest.setSystemTime(new Date('2026-08-05T12:00:00.000Z'));
+    openmrs.searchServiceRequests.mockResolvedValue({ entry: [] });
+    await orderPoller.pollOnce();
+
+    jest.setSystemTime(new Date('2026-08-05T12:00:15.000Z'));
+    await orderPoller.pollOnce();
+
+    expect(openmrs.searchServiceRequests).toHaveBeenNthCalledWith(2, {
+      lastUpdatedAfter: '2026-08-05T12:00:00.000Z'
+    });
+  });
+
+  test('the cursor still advances even when the OpenMRS search itself throws', async () => {
+    jest.setSystemTime(new Date('2026-08-05T12:00:00.000Z'));
+    openmrs.searchServiceRequests.mockRejectedValueOnce(new Error('OpenMRS unreachable'));
+    await orderPoller.pollOnce();
+
+    jest.setSystemTime(new Date('2026-08-05T12:00:15.000Z'));
+    openmrs.searchServiceRequests.mockResolvedValueOnce({ entry: [] });
+    await orderPoller.pollOnce();
+
+    expect(openmrs.searchServiceRequests).toHaveBeenNthCalledWith(2, {
+      lastUpdatedAfter: '2026-08-05T12:00:00.000Z'
+    });
+  });
+
+  test('start() schedules pollOnce on the given interval', async () => {
+    openmrs.searchServiceRequests.mockResolvedValue({ entry: [] });
+
+    orderPoller.start(15000);
+
+    expect(openmrs.searchServiceRequests).not.toHaveBeenCalled();
+    await jest.advanceTimersByTimeAsync(15000);
+    expect(openmrs.searchServiceRequests).toHaveBeenCalledTimes(1);
+
+    orderPoller.stop();
+  });
+
+  test('calling start() twice does not schedule a second timer', async () => {
+    openmrs.searchServiceRequests.mockResolvedValue({ entry: [] });
+
+    orderPoller.start(15000);
+    orderPoller.start(15000);
+    await jest.advanceTimersByTimeAsync(15000);
+
+    expect(openmrs.searchServiceRequests).toHaveBeenCalledTimes(1);
+
+    orderPoller.stop();
+  });
+
+  test('stop() clears the timer so pollOnce is not called again', async () => {
+    openmrs.searchServiceRequests.mockResolvedValue({ entry: [] });
+
+    orderPoller.start(15000);
+    orderPoller.stop();
+    await jest.advanceTimersByTimeAsync(15000);
+
+    expect(openmrs.searchServiceRequests).not.toHaveBeenCalled();
+  });
+});
